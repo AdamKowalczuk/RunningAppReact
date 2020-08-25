7c3d0bf376bde46bccf67e06d3baa4c863ae1cdb
Adam <adam.kowalczuk@poczta.fm>
2020-08-25 17:42:17 +0200

    message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router } from "@reach/router";
+import logo from "./logo.svg";
+import Home from "./pages/Home.js";
+import Calculator from "./pages/Calculator.js";
+import PaceConverter from "./pages/PaceConverter.js";
+import ResponsiveNavigation from "./components/ResponsiveNavigation";
+import "./App.css";
 
 function App() {
+  const navLinks = [
+    {
+      text: "Strona główna",
+      path: "/",
+      icon: "ion-ios-home",
+    },
+    {
+      text: "Kalkulator tempa biegu",
+      path: "/kalkulator-tempa-biegu",
+      icon: "ion-ios-calculator",
+    },
+    {
+      text: "Przelicznik",
+      path: "/przelicznik",
+      icon: "fas fa-exchange-alt",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ResponsiveNavigation
+        navLinks={navLinks}
+        logo={logo}
+        background="#CF395F"
+        hoverBackground="#342b57"
+        linkColor="#fff"
+      />
+      <Router>
+        <Home exact path="/" />
+        <Calculator path="/kalkulator-tempa-biegu" />
+        <PaceConverter path="/przelicznik" />
+      </Router>
     </div>
   );
 }
---
+++ src/pages/PaceConverter.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class PaceConverter extends Component {
+  render() {
+    return <div>Pace Converter</div>;
+  }
+}
